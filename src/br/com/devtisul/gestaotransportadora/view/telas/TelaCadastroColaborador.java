976b5e088589bcedd18ccbbfc71c1cabe22c7137
Bianca0909 <biancamedeiros0909@gmail.com>
2022-08-08 20:55:40 -0300

    importado jdbc

--- src/br/com/devtisul/gestaotransportadora/view/telas/TelaCadastroColaborador.java
+++ src/br/com/devtisul/gestaotransportadora/view/telas/TelaCadastroColaborador.java
@@ -8,12 +8,12 @@ package br.com.devtisul.gestaotransportadora.view.telas;
  *
  * @author usuario
  */
-public class TElaCadastroColaborador extends javax.swing.JFrame {
+public class TelaCadastroColaborador extends javax.swing.JFrame {
 
     /**
      * Creates new form TElaCadastroColaborador
      */
-    public TElaCadastroColaborador() {
+    public TelaCadastroColaborador() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class TElaCadastroColaborador extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TElaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TElaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TElaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TElaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TElaCadastroColaborador().setVisible(true);
+                new TelaCadastroColaborador().setVisible(true);
             }
         });
     }
